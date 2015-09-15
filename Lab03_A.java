785490749466001a3eae5c802df359604442f3d2
AnshTina <ansh.tina@hotmail.com>
2015-09-15 16:44:43 -0700

    Create Lab03_A.java

---
+++ Lab03_A.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Lab03_A {
+
+   
+    public static void main(String[] args) {
+   
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many pages to fax? ");
+        double pages = sc.nextInt();
+        double sCharge = 3;
+        double numPages = .20;
+        double cost;
+        
+        if (pages<=10)
+        {
+        cost= 3+pages*.20;
+        System.out.println("That will cost $" + cost);
+        }
+        
+        else 
+            if (pages>=10)
+                    {
+                    cost= 3+10*0.2+(pages-10)*0.1;
+                    System.out.println("That will cost $" + cost);
+                    }
+    }
+}
+                
